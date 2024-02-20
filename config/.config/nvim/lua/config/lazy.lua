2c164a8387d7be6bdab9cd91950a5226fef071eb
Jack Gardner <stonefeet@gmail.com>
2024-02-20 01:59:50 +0000

    test

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -26,14 +26,12 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true,
     notify = false,
     frequency = 86400,
   }, -- automatically check for plugin updates
   install = { colorscheme = { "tokyonight", "habamax", "catppuccin" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
