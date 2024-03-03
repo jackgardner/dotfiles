f30f64a4876e0e42f858db7bf4bd5558ab3f2c0f
Jack Gardner <stonefeet@gmail.com>
2024-03-03 19:48:23 +0000

    bump

---
+++ config/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,25 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'github/copilot.vim',
+  {
+    'nvim-neotest/neotest',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-neotest/neotest-jest' },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-jest' {
+            jestCommand = 'npm test --',
+            jestConfigFile = 'jest.config.js',
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          },
+        },
+      }
+    end,
+  },
+}
