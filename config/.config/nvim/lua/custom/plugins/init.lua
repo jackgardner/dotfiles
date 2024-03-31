c58eb784604c27a597c6019866e65f9d2d79be8e
Jack Gardner <stonefeet@gmail.com>
2024-03-31 19:32:20 +0100

    updates

--- config/.config/nvim/lua/custom/plugins/init.lua
+++ config/.config/nvim/lua/custom/plugins/init.lua
@@ -12,6 +12,22 @@ vim.keymap.set('n', '<leader>x', ':bd<cr>')
 -- See the kickstart.nvim README for more information
 return {
   {
+    'akinsho/toggleterm.nvim',
+    config = function()
+      require('toggleterm').setup {}
+
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float' }
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+    end,
+    keys = {
+      { '<leader>lg', '<Cmd>lua _lazygit_toggle()<CR>', mode = 'n' },
+    },
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     version = '*',
     lazy = false,
